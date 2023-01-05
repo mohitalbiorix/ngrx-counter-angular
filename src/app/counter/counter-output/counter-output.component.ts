82914dd64c9b61a4cbbd8079acae61941fae7797
Ankur <ankur.akvaliya@acearea.com>
2023-01-05 18:54:17 +0530

    ngrx counter demo

---
+++ src/app/counter/counter-output/counter-output.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { Store } from "@ngrx/store";
+import { Observable } from 'rxjs';
+import { CounterState } from '../state/counter.state';
+
+@Component({
+  selector: 'app-counter-output',
+  templateUrl: './counter-output.component.html',
+  styleUrls: ['./counter-output.component.scss']
+})
+export class CounterOutputComponent implements OnInit {
+
+  counter!: number;
+  counter$! : Observable<any>;
+
+  constructor(
+    private store: Store<{ counter: CounterState}>
+  ) { }
+
+  ngOnInit(): void {
+    this.counter$ = this.store.select('counter');
+  }
+
+}
---
+++ src/app/counter/state/counter.state.ts
@@ -0,0 +1,6 @@
+export interface CounterState {
+    counter:number
+}
+export const initialState : CounterState = {
+    counter: 0
+}
