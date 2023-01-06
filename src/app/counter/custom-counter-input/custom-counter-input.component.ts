cefa47963c41b320f99579829cfca2eea178c8f1
Ankur <ankur.akvaliya@acearea.com>
2023-01-06 12:51:54 +0530

    Worked on pass custom data using props

---
+++ src/app/counter/custom-counter-input/custom-counter-input.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { customIncrement } from '../state/counter.actions';
+import { CounterState } from '../state/counter.state';
+
+@Component({
+  selector: 'app-custom-counter-input',
+  templateUrl: './custom-counter-input.component.html',
+  styleUrls: ['./custom-counter-input.component.scss']
+})
+export class CustomCounterInputComponent implements OnInit {
+
+  num!: number
+  constructor(
+    private store: Store<{ counter: CounterState }>
+  ) { }
+
+  ngOnInit(): void {
+  }
+
+  addNumber() {
+    this.store.dispatch(customIncrement({ count: +this.num }))
+  }
+
+}
