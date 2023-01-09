91d54808df588ada74bbcaffa2cf6e2c0c17609c
Mohit Kapadiya <mohitk.albiorix@gmail.com>
2023-01-09 11:52:50 +0530

    Worked on create api of getpost based on id and set value of edit form.

--- src/app/posts/add-post/add-post.component.ts
+++ src/app/posts/add-post/add-post.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Post } from 'src/app/model/posts.model';
+import { AppState } from 'src/app/store/app.state';
+import { add_post } from '../state/posts.actions';
+import { getPostById } from '../state/posts.selectors';
 
 @Component({
   selector: 'app-add-post',
@@ -9,20 +15,36 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class AddPostComponent implements OnInit {
 
   postForm!: FormGroup;
+  post!: Post
+  action: string ='ADD'
 
-  constructor() { }
+  constructor(
+    private store:Store<AppState>,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    this.route.queryParams.subscribe(
+      query => {
+        const postId = query['postId'];
+        this.action = query['action'];
+        this.store.select(getPostById, { postId }).subscribe(
+          data => {
+            this.post = data;
+          }
+        )
+      }
+    )
     this.initForm();
   }
 
-  initForm(){
+  initForm() {
     this.postForm = new FormGroup({
-      title: new FormControl(null, [
+      title: new FormControl(this.action === 'EDIT' ? this.post.title : null, [
         Validators.required,
         Validators.minLength(6),
       ]),
-      description: new FormControl(null, [
+      description: new FormControl(this.action === 'EDIT' ? this.post.description : null, [
         Validators.required,
         Validators.minLength(10),
       ]),
@@ -33,18 +55,27 @@ export class AddPostComponent implements OnInit {
     const descriptionForm = this.postForm.get('description');
     if (descriptionForm?.touched && !descriptionForm.valid) {
       if (descriptionForm?.errors?.['required']) {
-        return 'Description is required';
+        return '* Description is required';
       }
       if (descriptionForm?.errors?.['minlength']) {
-        return 'Description should be of minimum 10 characters length';
+        return '* Description should be of minimum 10 characters length';
       }
     }
     return;
   }
   
 
-  onAddPost(){
-    console.log(this.postForm);
+  onAddPost() {
+    this.postForm.markAllAsTouched();
+    if (!this.postForm.valid) {
+      return;
+    }
+    const post: Post = {
+      title: this.postForm.value.title,
+      description: this.postForm.value.description
+    }
+
+    this.store.dispatch(add_post({ post }))
   }
 
 }
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { Post } from 'src/app/model/posts.model';
@@ -15,11 +16,21 @@ export class PostListComponent implements OnInit {
   posts! :Observable<Post[]>;
 
   constructor(
-    private store: Store<AppState>
+    private store: Store<AppState>,
+    private router:Router
   ) { }
 
   ngOnInit(): void {
     this.posts = this.store.select(getPosts);
+  }
+
+  goEditPage(postId: any) {
+    this.router.navigate(['post-list/add'], {
+      queryParams: {
+        postId: postId,
+        action: 'EDIT'
+      }
+    })
   }
 
 }
