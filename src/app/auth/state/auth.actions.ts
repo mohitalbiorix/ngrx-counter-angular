7b80b64c5ae2dbcffed93af3071a03de0eab172a
Mohit Kapadiya <mohitk.albiorix@gmail.com>
2023-01-16 17:38:03 +0530

    Implement  auto logout functionality.

--- src/app/auth/state/auth.actions.ts
+++ src/app/auth/state/auth.actions.ts
@@ -7,17 +7,20 @@ export const LOGIN_FAIL = '[auth page] login fail';
 export const SIGNUP_START = '[auth page] signup start';
 export const SIGNUP_SUCCESS = '[auth page] signup success';
 export const AUTO_LOGIN = '[auth page] auto login';
+export const AUTO_LOGOUT = '[auth page] auto logout';
 
 export const loginStart = createAction(
     LOGIN_START, props<{ email: string, password: string }>());
 
 export const loginSuccess = createAction(
-    LOGIN_SUCCESS, props<{ user: User }>())
+    LOGIN_SUCCESS, props<{ user: User, redirect: boolean }>());
 
 export const signupStart = createAction(
-    SIGNUP_START, props<{ email: string, password: string }>())
+    SIGNUP_START, props<{ email: string, password: string }>());
 
 export const signupSuccess = createAction(
-    SIGNUP_SUCCESS, props<{ user: User }>())
+    SIGNUP_SUCCESS, props<{ user: User, redirect: boolean }>());
 
-export const autoLogin = createAction(AUTO_LOGIN)
+export const autoLogin = createAction(AUTO_LOGIN);
+
+export const autoLogout = createAction(AUTO_LOGOUT);
